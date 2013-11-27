c5e827bb5aa2b1aa369d5fa8f1353822e47690e3
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-11-27 09:49:10 +0900

    support username

--- 20131120-phonebook/hsbt/phone_book.rb
+++ 20131120-phonebook/hsbt/phone_book.rb
@@ -1,13 +1,15 @@
 class PhoneBook
   attr_reader :numbers
   def initialize(str = nil)
-    @numbers = str.split("\n")
+    @numbers = str.split("\n").map{|n| n.split(':').map{|v| v.strip} }
   end
 
   def collision?(index)
     master = @numbers[index]
     subset = @numbers.dup
     subset.delete(master)
-    subset.inject(false){|r, num| r || (master.include?(num))}
+    subset.inject(false) do |r, num|
+      r || (master[1].match(/\A#{num[1]}/))
+    end
   end
 end
--- 20131120-phonebook/hsbt/phone_book_test.rb
+++ 20131120-phonebook/hsbt/phone_book_test.rb
@@ -3,17 +3,24 @@ require_relative './phone_book'
 
 class PhoneBookTest < Test::Unit::TestCase
   def test_initialize
-    book = PhoneBook.new("000\n111\n222\n")
+    book = PhoneBook.new("alpha: 000\nbravo: 111\ncharlie: 222\n")
     assert book.numbers
-    assert_equal "000", book.numbers[0]
-    assert_equal "111", book.numbers[1]
-    assert_equal "222", book.numbers[2]
+    assert_equal "000", book.numbers[0][1]
+    assert_equal "111", book.numbers[1][1]
+    assert_equal "222", book.numbers[2][1]
   end
 
   def test_collision?
-    book = PhoneBook.new("000\n111\n111222\n")
+    book = PhoneBook.new("alpha: 000\nbravo: 111\ncharlie: 111222\n")
     assert !book.collision?(0)
     assert !book.collision?(1)
     assert book.collision?(2)
+  end
+
+  def test_collision_2
+    book = PhoneBook.new("alpha: 000\nbravo: 111\ncharlie: 222111\n")
+    assert !book.collision?(0)
+    assert !book.collision?(1)
+    assert !book.collision?(2)
   end
 end
