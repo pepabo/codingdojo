1bbc4f6ff487a1a5e918a84bcb69dccb4ef28e85
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-12-18 09:48:24 +0900

    test を分割した

--- 20131211-trivia/test/game_test.rb
+++ 20131211-trivia/test/game_test.rb
@@ -6,12 +6,6 @@ class GameTest < MiniTest::Unit::TestCase
 
   def test_initialize
     game = UglyTrivia::Game.new
-    assert game.add 'Chet'
-    assert_equal ["Chet"], game.players
-    assert_equal 1, game.how_many_players
-    assert_equal 0, game.places[1]
-    assert_equal 0, game.purses[1]
-    assert !game.in_penalty_box[1]
 
     assert_equal 50, game.pop_questions.size
 
@@ -19,6 +13,16 @@ class GameTest < MiniTest::Unit::TestCase
     assert_equal "Science Question 49", game.science_questions.last
     assert_equal "Sports Question 49", game.sports_questions.last
     assert_equal "Rock Question 49", game.rock_questions.last
+  end
+
+  def test_add
+    game = UglyTrivia::Game.new
+    assert game.add 'Chet'
+    assert_equal ["Chet"], game.players
+    assert_equal 1, game.how_many_players
+    assert_equal 0, game.places[1]
+    assert_equal 0, game.purses[1]
+    assert !game.in_penalty_box[1]
   end
 
   def test_how_many_players
